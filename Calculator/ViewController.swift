f0bc01158eda19cf23b9242fe5879379fca0cd1e
Jorge Gallardo <jorge8486@gmail.com>
2015-08-19 00:16:54 -0400

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jorge Gallardo on 8/19/15.
+//  Copyright © 2015 Jorge Gallardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func numberPress(sender: UIButton) {
+    }
+}
+
