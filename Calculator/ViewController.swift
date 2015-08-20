3fbcdfb1c28b73f0176ab38dfa20da20f1aaf21f
Jorge Gallardo <jorge8486@gmail.com>
2015-08-19 21:23:40 -0400

    added the decimal point, sin, and cos functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
-    var userIsTypingNumber = false
+    @IBOutlet weak var decimalPoint: UIButton!
+    var userIsInTheMiddleOfTypingANumber = false
     var numbersArray = [Double]()
     var displayValue: Double {
         get {
@@ -18,41 +19,55 @@ class ViewController: UIViewController {
         }
         set {
             display.text = "\(newValue)"
-            userIsTypingNumber = false
+            userIsInTheMiddleOfTypingANumber = false
         }
     }
     
     @IBAction func numberPress(sender: UIButton) {
         let digit = sender.currentTitle!
         
-        if userIsTypingNumber {
-            display.text! += digit
+        if userIsInTheMiddleOfTypingANumber {
+            if digit == "." {
+                display.text! += digit
+                decimalPoint.enabled = false
+            } else {
+                display.text! += digit
+            }
         } else {
             display.text = digit
-            userIsTypingNumber = true
+            if digit == "." {
+                decimalPoint.enabled = false
+            }
+            userIsInTheMiddleOfTypingANumber = true
         }
     }
     
     @IBAction func operate(sender: UIButton) {
+        decimalPoint.enabled = true
         let operation = sender.currentTitle!
         
-        if userIsTypingNumber {
+        if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         
         switch operation {
-        case "×": performOperation({ (op1: Double, op2: Double) -> Double in
-            return op1 * op2
-        })
+            case "×": performOperation({ (op1: Double, op2: Double) -> Double in
+                return op1 * op2
+            })
             
-        case "÷": performOperation({ (op1, op2) in return op2 / op1 })
+            case "÷": performOperation({ (op1, op2) in return op2 / op1 })
             
-        case "−": performOperation({$1 - $0})
+            case "−": performOperation({$1 - $0})
             
-        case "+": performOperation() {$0 + $1} // or performOperation {$0+S1} because there are no other arguments
+            case "+": performOperation() {$0 + $1} // or performOperation {$0+S1} because there are no other arguments
             
-        case "√": performOperation { sqrt($0) }
-        default: break
+            case "√": performOperation { sqrt($0) }
+            
+            case "sin": performOperation { sin($0) }
+            
+            case "cos": performOperation { cos($0) }
+            
+            default: break
         }
     }
     
@@ -71,8 +86,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func enter() { // no arguments because there's only 1 enter key
-        userIsTypingNumber = false
-        
+        userIsInTheMiddleOfTypingANumber = false
+        decimalPoint.enabled = true
         numbersArray.append(displayValue)
         print(numbersArray)
     }
